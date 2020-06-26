f0d28662fe6ee9099dc4cd714d4017847ebc9fdf
Alina Phang <aph78@uclive.ac.nz>
2020-06-26 13:07:34 +1200

    Fixed some quotes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-let quotes = ["Protection and power are overrated. I think you are very wise to choose happiness and love.",
+let quotes = ["Perfection and power are overrated. I think you are very wise to choose happiness and love.",
     "Sometimes life is like this dark tunnel, you can’t always see the light at the end of the tunnel, but if you just keep moving, you will come to a better place.",
-    "It is important to draw wisdom from different places. If you take it from only one place it become rigid and stale.",
-    "Pride is not the opposite of shame, but it’s source. True humility is the only antidote to shame.",
+    "It is important to draw wisdom from different places. If you take it from only one place it becomes rigid and stale.",
+    "Pride is not the opposite of shame, but its source. True humility is the only antidote to shame.",
     "Life happens wherever you are, whether you make it or not.",
-    "While it is always best to believe in one’s self, a little help from others can be a great blessing.",
+    "While it is always best to believe in oneself, a little help from others can be a great blessing.",
     "A man needs his rest.",
     "It is usually best to admit mistakes when they occur, and to seek to restore honor."];
 
